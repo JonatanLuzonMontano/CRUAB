92aa5cfc53ed79b156db6ecbbc87c95bac5e11b0
5e_Cleric <losada.hernandez.victor@gmail.com>
2022-08-10 23:03:07 +0200

    actualizada pagina membre/usuari

--- js/cssafegits.js
+++ js/cssafegits.js
@@ -2,25 +2,41 @@
 window.onload = addevents;
 
 function addevents() {
-    var navbuttons = document.getElementsByClassName("nav-link");
-    for (i = 0; i < navbuttons.length; i++) {
-        navbuttons[i].addEventListener('mousedown', clickeffect);
-        navbuttons[i].addEventListener('mouseup', clickeffect);
-    }
+  var navbuttons = document.getElementsByClassName("nav-link");
+  for (i = 0; i < navbuttons.length; i++) {
+    navbuttons[i].addEventListener('mousedown', clickeffect);
+    navbuttons[i].addEventListener('mouseup', clickeffect);
+  }
 
-    document.getElementById("navbar-toggler").addEventListener('click', opennav);
-    document.getElementById("theme-toggler").addEventListener('click', changetheme);
+  document.getElementById("navbar-toggler").addEventListener('click', opennav);
+  document.getElementById("theme-toggler").addEventListener('click', changetheme);
 }
 
 function clickeffect(e) {
-    e.target.classList.toggle("clicked");
+  e.target.classList.toggle("clicked");
 }
 
-
 function opennav() {
-    document.getElementById("navbuttons").classList.toggle("collapsed");
+  document.getElementById("navbuttons").classList.toggle("collapsed");
 }
 
 function changetheme() {
-    document.body.classList.toggle("black-mode");
+  document.body.classList.toggle("black-mode");
+}
+
+function toggleuserpagecontent(id) {
+  console.log(id);
+  document.querySelectorAll(".user-content-article").forEach(element => {
+    if (element.classList.contains("open")) {
+      element.classList.remove("open");
+    }
+  });
+  document.getElementsByClassName(id)[0].classList.toggle("open");
+  document.querySelectorAll(".user-button").forEach(element => {
+    if (element.classList.contains("active")) {
+      element.classList.remove("active");
+    }
+  });
+  document.getElementById(id).classList.toggle("active");
+  
 }
