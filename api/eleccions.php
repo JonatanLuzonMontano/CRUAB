a01f10b943984700f226af209ec58ffbad943cbd
JonatanLuzonMontano <pompa-jona@hotmail.com>
2022-08-28 01:00:06 +0200

    creado el modulo eleccions.php

---
+++ api/eleccions.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+include_once('../php/connexiobd.php');
+include_once('../php/seguretat.php');
+
+switch($_SERVER['REQUEST_METHOD']){
+    
+    case 'GET':
+        
+        break;
+
+    case 'PUT':
+    
+        break;
+
+    case 'POST':
+        
+        break;
+
+    case 'DELETE':
+        
+        break;
+
+    default:
+        http_response_code(400);
+        echo "wrong method";
+        break;
+}
+
+echo json_encode($msg, JSON_NUMERIC_CHECK, JSON_UNESCAPED_UNICODE);
+
+?>
