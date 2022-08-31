6334b56c88cf2273f2e93018ec032530907fcc78
5e_Cleric <losada.hernandez.victor@gmail.com>
2022-08-31 21:54:50 +0200

    eleccions PUT añadido, prototipo post, logo svg

--- js/gestio.js
+++ js/gestio.js
@@ -1,38 +1,18 @@
 
-function obrirLlistes() {
+function obrirEleccions() {
   if (confirm("Estas segur?")) {
-    alert("obrint periode de presentacio de llistes");
-
-
-
-
-
-    //aqui hay que ahcer una http request con un 
-    //put o post de llistes para hacer que nignuna sea ganadora
-  }
-}
-
-function votacio() {
-  var data = {};
-
-  data['pas'] = "votacio";
-
-  var xhttpvotacio = new XMLHttpRequest();
-  xhttpvotacio.onreadystatechange = function () {
-    var data = JSON.parse(xhttp.responseText);
-    if ((data.hasOwnProperty('Error'))) {
-      document.getElementById('missatge').innerHTML = `<p>${data["Error"]}</p>`;
-      if (data.hasOwnProperty('DeBug')) {
-        document.getElementById('missatge').innerHTML += `<p>${data["DeBug"]}</p>`;
+    var xhttpobrireleccions = new XMLHttpRequest;
+    xhttpobrireleccions.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
       }
-    } else {
-      location.reload();
-    }
-    data['pas'] = "votacio";
+    };
+    xhttpobrireleccions.open('PUT', '/api/eleccions.php', true);
+    xhttpobrireleccions.send();
   }
-  xhttpcheckeleccions.open('PUT', '/api/eleccions.php', true);
-  xhttpcheckeleccions.send(JSON.stringify(data));
 }
+
+
 
 
 
--- js/lloguerjocs.js
+++ js/lloguerjocs.js
@@ -65,7 +65,7 @@ function retornaLlogerJoc(numlloguer){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            location.reload();
+          location.reload();
         }
     };
     xhttp.open('DELETE', '/api/lloguerjoc.php?numlloguer='+numlloguer, true);
