5ccd3617e1584c8adbe608d6c6b4c65932c69865
5e_Cleric <losada.hernandez.victor@gmail.com>
2022-09-01 18:40:43 +0200

    botones casos put elecciones

--- js/gestio.js
+++ js/gestio.js
@@ -12,6 +12,96 @@ function obrirEleccions() {
   }
 }
 
+function obrirLlistes() {
+  if (confirm("Estas segur?")) {
+    var xhttpobrirllistes = new XMLHttpRequest;
+    xhttpobrirllistes.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttpobrirllistes.open('PUT', '/api/eleccions.php', true);
+    xhttpobrirllistes.send();
+  }
+}
+
+function obrirVotacio() {
+  if (confirm("Estas segur?")) {
+    var xhttpobrirvotacio = new XMLHttpRequest;
+    xhttpobrirvotacio.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttpobrirvotacio.open('PUT', '/api/eleccions.php', true);
+    xhttpobrirvotacio.send();
+  }
+}
+function obrirActivacio() {
+  if (confirm("Estas segur?")) {
+    var xhttpobriractivacio = new XMLHttpRequest;
+    xhttpobriractivacio.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttpobriractivacio.open('PUT', '/api/eleccions.php', true);
+    xhttpobriractivacio.send();
+  }
+}
+//#####################################   TANCAR   ##################################################
+
+function tancarEleccions() {
+  if (confirm("Estas segur?")) {
+    var xhttptancareleccions = new XMLHttpRequest;
+    xhttptancareleccions.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttptancareleccions.open('PUT', '/api/eleccions.php', true);
+    xhttptancareleccions.send();
+  }
+}
+
+function tancarLlistes() {
+  if (confirm("Estas segur?")) {
+    var xhttptancarllistes = new XMLHttpRequest;
+    xhttptancarllistes.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttptancarllistes.open('PUT', '/api/eleccions.php', true);
+    xhttptancarllistes.send();
+  }
+}
+
+function tancarVotacio() {
+  if (confirm("Estas segur?")) {
+    var xhttptancarvotacio = new XMLHttpRequest;
+    xhttptancarvotacio.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttptancarvotacio.open('PUT', '/api/eleccions.php', true);
+    xhttptancarvotacio.send();
+  }
+}
+function tancarActivacio() {
+  if (confirm("Estas segur?")) {
+    var xhttptancaractivacio = new XMLHttpRequest;
+    xhttptancaractivacio.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        location.reload();
+      }
+    };
+    xhttptancaractivacio.open('PUT', '/api/eleccions.php', true);
+    xhttptancaractivacio.send();
+  }
+}
+
 
 
 
